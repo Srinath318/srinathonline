db5bafd7fa311494bf1cbe220a64103480354e10
Srinath318 <187srinath@gmail.com>
2025-10-16 19:54:27 +0530

    your message

---
+++ script.js
@@ -0,0 +1,45 @@
+let container = document.querySelector(".container");
+let loader = document.querySelector(".loader");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const typewriter = document.getElementById("typewriter");
+const text = "Srinath";
+let index = 0;
+
+window.onload = () => {
+  // Show loader for 2 seconds
+  setTimeout(() => {
+    loader.style.display = "none";
+    container.style.display = "block";
+
+    // Start typewriter after loader disappears
+    type();
+  }, 2000);
+
+  // Button click
+  button.addEventListener("click", () => {
+    let value = input.value.trim().toLowerCase();
+    if (value === "show portfolio") {
+      input.value = "";
+      window.location.href = "portfolio.html";
+    } else {
+      alert("Type 'show portfolio' to go to the Portfolio");
+    }
+  });
+
+  // Enter key triggers search
+  input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      button.click();
+    }
+  });
+};
+
+// Typewriter function
+function type() {
+  if (index < text.length) {
+    typewriter.innerHTML += text[index];
+    index++;
+    setTimeout(type, 200); // typing speed
+  }
+}
